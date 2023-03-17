8f4121be433051f02132fc7e1e32b8d91023ede9
Aadv1k <aadv1k@outlook.com>
2023-03-17 21:45:45 +0530

    Add impl for getEpisode, getPodcast

---
+++ routes/GetPodcast.js
@@ -0,0 +1,30 @@
+const { ERR, MIME } = require("../app/constants");
+const { sendJsonErr, isCookieAndSessionValid} = require("../app/common");
+const cookie = require("cookie");
+const { UserModel } = require("../models/UserModel.js");
+const { Store } = require("../models/MemoryStore.js");
+const USER_DB = new UserModel();
+const querystring = require("querystring");
+
+
+module.exports = async (req, res) => {
+  await USER_DB.init();
+  if (!isCookieAndSessionValid(req)) {
+    sendJsonErr(res, ERR.unauthorized);
+    return;
+  }
+
+  const { podcastID } = querystring.parse(req.url.split('?').pop());
+  const userid = Store.get(cookie.parse(req.headers.cookie).sessionid).uid;
+
+  const podcast = await USER_DB.getPodcastForUser({_id: userid}, podcastID);
+
+  if (podcast) {
+    res.writeHead(200, {
+      "Content-type": MIME.json
+    })
+    res.write(JSON.stringify(podcast ?? {}));
+    res.end();
+    return;
+  }
+}
